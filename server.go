cfb0bb447e77b08f4e74810b3198827611007728
yumekiti <yumekiti120440@gmail.com>
2022-08-02 15:48:17 +0900

    add middleware

--- server.go
+++ server.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 	
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
