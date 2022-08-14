4ed8bb4a4f19be642e96a5e05a2cdeb8cf3b6019
yumekiti <yumekiti120440@gmail.com>
2022-08-14 23:11:50 +0900

    `chenge` main

--- api/main.go
+++ api/main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/yumekiti/echo-demo/config"
+	"github.com/yumekiti/echo-demo/infrastructure"
+	"github.com/yumekiti/echo-demo/interface/handler"
+	"github.com/yumekiti/echo-demo/usecase"
 )
 
 func main() {
+	taskRepository := infrastructure.NewTaskRepository(config.NewDB())
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskHandler := handler.NewTaskHandler(taskUsecase)
+
 	e := echo.New()
 
 	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
+	handler.InitRouting(e, taskHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- infrastructure/task.go
+++ infrastructure/task.go
@@ -1,4 +1,4 @@
-package infra
+package infrastructure
 
 import (
 	"github.com/yumekiti/echo-demo/domain/model"
