750fe20c045c9b6fcab54017a9a5e8a68ed91c52
yumekiti <yumekiti120440@gmail.com>
2022-08-14 20:48:01 +0900

    `add` task

---
+++ domain/model/task.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"errors"
+
+	"github.com/yumekiti/echo-demo/entity"
+)
+
+// NewTask taskのコンストラクタ
+func NewTask(title, context string) (*entity.Task, error) {
+	if title == "" {
+		return nil, errors.New("titleを入力してください")
+	}
+
+	task := &entity.Task{
+		Title:   title,
+		Context: context,
+	}
+
+	return task, nil
+}
+
+// Set taskのセッター
+func (t *entity.Task) Set(title, context string) error {
+	if title == "" {
+		return errors.New("titleを入力してください")
+	}
+
+	t.Title = title
+	t.Context = context
+
+	return nil
+}
